e4595cf86e83a7cf034fca9032349cd0cfe5bd6f
phoenix <1392100700@qq.com>
2016-12-03 20:43:10 +0800

    第二次提交

---
+++ app/entry.js
@@ -0,0 +1,3 @@
+document.getElementById("app").innerHTML = "这是我的第一个webpack打包成功的程序";
+require("./first.js");
+require("./style.css");
---
+++ app/first.js
@@ -0,0 +1,3 @@
+var h2 = document.createElement("h2");
+h2.innnerHtml = "不是吧，这么快就打包第二个程序啦";
+document.body.appendChild(h2);
